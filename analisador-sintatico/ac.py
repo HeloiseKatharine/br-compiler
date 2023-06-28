594e502ecc6b79df70df8ca7228986206a65a7e1
josefreitas788 <josefreitas788@gmail.com>
2023-06-27 21:49:41 -0300

    atualizando gramatica

--- analisador-sintatico/ac.py
+++ analisador-sintatico/ac.py
@@ -21,20 +21,24 @@ def create_ac_grammar()->Grammar:
     G.add_production('StmtIf',['endif']) #12
     G.add_production('StmtIf',['else','Stmt','Stmts','endif']) #13
     G.add_production('ExprLogica',['Expr','Comparador', 'Expr']) #14
-    G.add_production('Comparador',['maior','maiorIgual', 'menor', 'menorIgual', 'igual', 'diferente']) #15
-    G.add_production('Expr',['Termo','Expr1']) #16
-    G.add_production('Expr1',['adicao','Termo','Expr1']) #17
-    G.add_production('Expr1',['subtracao','Termo','Expr1']) #18
-    G.add_production('Expr1',[]) #19
-    G.add_production('Termo',['Fator','Termo1']) #20
-    G.add_production('Termo1',['multiplicacao','Fator','Termo1']) #21
-    G.add_production('Termo1',['divisao','Fator','Termo1']) #22
-    G.add_production('Termo1',[]) #23
-    G.add_production('Fator',['id']) #24
-    G.add_production('Fator',['inum']) #25
-    G.add_production('Fator',['fnum']) #26
-    G.add_production('Fator',['(','Expr',')']) #27
-
+    G.add_production('Comparador',['maior']) #15
+    G.add_production('Comparador',['maiorIgual']) #16
+    G.add_production('Comparador',['menor']) #17
+    G.add_production('Comparador',['menorIgual']) #18
+    G.add_production('Comparador',['igual']) #19
+    G.add_production('Comparador',['diferente']) #20
+    G.add_production('Expr',['Termo','Expr1']) #21
+    G.add_production('Expr1',['adicao','Termo','Expr1']) #22
+    G.add_production('Expr1',['subtracao','Termo','Expr1']) #23
+    G.add_production('Expr1',[]) #24
+    G.add_production('Termo',['Fator','Termo1']) #25
+    G.add_production('Termo1',['multiplicacao','Fator','Termo1']) #26
+    G.add_production('Termo1',['divisao','Fator','Termo1']) #27
+    G.add_production('Termo1',[]) #28
+    G.add_production('Fator',['id']) #29
+    G.add_production('Fator',['inum']) #30
+    G.add_production('Fator',['fnum']) #31
+    G.add_production('Fator',['(','Expr',')']) #32
     
     G.add_terminal('floatdcl')
     G.add_terminal('intdcl')
@@ -159,40 +163,69 @@ def Stmts(ts:token_sequence, p:predict_algorithm)->None:
 
 def Stmt(ts:token_sequence, p:predict_algorithm)->None:
     if ts.peek() in p.predict(8):
-        ts.match('print')
-        Expr(ts,p)
-    elif ts.peek() in p.predict(9):
         ts.match('id')
         ts.match('assign')
         Expr(ts,p)
+    elif ts.peek() in p.predict(9):
+        ts.match('while')
+        Expr(ts,p)
+        ts.match('do')
+        Stmt(ts,p)
+        Stmts(ts,p)
+        ts.match('endwhile')
     elif ts.peek() in p.predict(10):
         ts.match('if')
         Expr(ts,p)
         ts.match('then')
+        Stmt(ts,p)
         Stmts(ts,p)
         StmtIf(ts,p)
     elif ts.peek() in p.predict(11):
-        ts.match('while')
+        ts.match('print')
         Expr(ts,p)
-        ts.match('do')
-        Stmts(ts,p)
-        ts.match('endwhile')
     else:
         print('Syntax error: ',ts.peek())
         exit(0)
-
 def StmtIf(ts:token_sequence, p:predict_algorithm)->None:
     if ts.peek() in p.predict(12):
-        ts.match('else')
-        Stmts(ts,p)
+        ts.match('endif')
     elif ts.peek() in p.predict(13):
+        ts.match('else')
+        Stmt(ts,p)
+        Stmts(ts,p)
         ts.match('endif')
     else:
         print('Syntax error: ',ts.peek())
         exit(0)
 
-def Expr(ts:token_sequence, p:predict_algorithm)->None:
+def ExprLogica(ts:token_sequence, p:predict_algorithm)->None:
     if ts.peek() in p.predict(14):
+        Expr(ts,p)
+        Comparador(ts,p)
+        Expr(ts,p)
+    else:
+        print('Syntax error: ',ts.peek())
+        exit(0)
+
+def Comparador(ts:token_sequence, p:predict_algorithm)->None:
+    if ts.peek() in p.predict(15):
+        ts.match('maior')
+    elif ts.peek() in p.predict(16):
+        ts.match('maiorIgual')
+    elif ts.peek() in p.predict(17):
+        ts.match('menor')
+    elif ts.peek() in p.predict(18):
+        ts.match('menorIgual')
+    elif ts.peek() in p.predict(19):
+        ts.match('igual')
+    elif ts.peek() in p.predict(20):
+        ts.match('diferente')
+    else:
+        print('Syntax error: ',ts.peek())
+        exit(0)
+
+def Expr(ts:token_sequence, p:predict_algorithm)->None:
+    if ts.peek() in p.predict(21):
         Termo(ts,p)
         Expr1(ts,p)
     else:
@@ -200,22 +233,22 @@ def Expr(ts:token_sequence, p:predict_algorithm)->None:
         exit(0)
 
 def Expr1(ts:token_sequence, p:predict_algorithm)->None:
-    if ts.peek() in p.predict(15):
+    if ts.peek() in p.predict(22):
         ts.match('adicao')
         Termo(ts,p)
         Expr1(ts,p)
-    elif ts.peek() in p.predict(16):
-        ts.match('subtracao')
+    elif ts.peek() in p.predict(23):
+        ts.match('subtracao'    )
         Termo(ts,p)
         Expr1(ts,p)
-    elif ts.peek() in p.predict(17):
+    elif ts.peek() in p.predict(24):
         return
     else:
         print('Syntax error: ',ts.peek())
         exit(0)
 
 def Termo(ts:token_sequence, p:predict_algorithm)->None:
-    if ts.peek() in p.predict(18):
+    if ts.peek() in p.predict(25):
         Fator(ts,p)
         Termo1(ts,p)
     else:
@@ -223,37 +256,34 @@ def Termo(ts:token_sequence, p:predict_algorithm)->None:
         exit(0)
 
 def Termo1(ts:token_sequence, p:predict_algorithm)->None:
-    if ts.peek() in p.predict(19):
+    if ts.peek() in p.predict(26):
         ts.match('multiplicacao')
         Fator(ts,p)
         Termo1(ts,p)
-    elif ts.peek() in p.predict(20):
+    elif ts.peek() in p.predict(27):
         ts.match('divisao')
         Fator(ts,p)
         Termo1(ts,p)
-    elif ts.peek() in p.predict(21):
+    elif ts.peek() in p.predict(28):
         return
     else:
         print('Syntax error: ',ts.peek())
         exit(0)
 
 def Fator(ts:token_sequence, p:predict_algorithm)->None:
-    if ts.peek() in p.predict(22):
+    if ts.peek() in p.predict(29):
         ts.match('id')
-    elif ts.peek() in p.predict(23):
+    elif ts.peek() in p.predict(30):
         ts.match('inum')
-    elif ts.peek() in p.predict(24):
+    elif ts.peek() in p.predict(31):
         ts.match('fnum')
-    elif ts.peek() in p.predict(25):
+    elif ts.peek() in p.predict(32):
         ts.match('(')
         Expr(ts,p)
         ts.match(')')
-    else:
-        print('Syntax error: ',ts.peek())
-        exit(0)
 
 if __name__ == '__main__':
-    #filepath = 'programa.br'
+    filepath = 'programa.br'
     #tokens = lexical_analyser(filepath)
     #ts = token_sequence(tokens)
     G = create_ac_grammar()
